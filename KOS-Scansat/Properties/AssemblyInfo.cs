9ec3df882c4dcf7ba9ea7669f874d94526a3bfae
Christian Bronk <chris.github@bronk.de>
2016-10-19 19:23:53 +0200

    Version update

--- KOS-Scansat/Properties/AssemblyInfo.cs
+++ KOS-Scansat/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("0.9.*")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 [assembly: KSPAssemblyDependency("kOS", 1, 0)]
